752e6a8100ee8d2e65e1a3a3ac405eb30afd82e3
arpitmotwani0708 <arpit12221@gmail.com>
2021-07-18 04:46:02 +0530

    Added some more operations on my doubly-linked-list

--- DoublyLinkedListExample.java
+++ DoublyLinkedListExample.java
@@ -79,6 +79,84 @@ temp=temp.getPrev();
 }
 System.out.println();
 }
+
+public void insertAtEnd(int data)
+{
+Node newNode = getNewNode(data);
+if(head==null)
+{
+head=newNode;
+return;
+}
+Node temp = head;
+while(temp.getNext()!=null)
+{
+temp=temp.getNext();
+}
+temp.setNext(newNode);
+newNode.setPrev(temp);
+}
+
+public void insertAtPosition(int data, int position)
+{
+Node newNode = getNewNode(data);
+if(head==null)
+{
+head=newNode;
+return;
+}
+if(position==1)
+{
+newNode.setNext(head);
+head.setPrev(newNode);
+head=newNode;
+return;
+}
+
+Node temp = head;
+int currentPosition = 1;
+while(currentPosition<position-1)
+{
+temp=temp.getNext();
+currentPosition++;
+}
+Node nextNode = temp.getNext();
+
+temp.setNext(newNode);
+newNode.setPrev(temp);
+
+if(nextNode!=null)
+{
+newNode.setNext(nextNode);
+nextNode.setPrev(newNode);
+}
+}
+
+public void deleteNthNode(int position)
+{
+Node temp = head;
+if(position==1)
+{
+head.getNext().setPrev(null);
+head=head.getNext();
+return;
+}
+
+int currentPosition=1;
+while(currentPosition<position-1)
+{
+temp=temp.getNext();
+currentPosition++;
+}
+
+Node nodeToDelete = temp.getNext();
+temp.setNext(nodeToDelete.getNext());
+if(nodeToDelete.getNext()!=null)
+{
+nodeToDelete.getNext().setPrev(temp);
+}
+
+}
 }
 
 class MainExecutor
@@ -97,5 +175,68 @@ doublyLinkedList.reversePrint();
 doublyLinkedList.insertAtBegining(7);
 doublyLinkedList.print();
 doublyLinkedList.reversePrint();
+
+System.out.println("============================");
+
+
+doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.insertAtEnd(2);
+doublyLinkedList.print();
+doublyLinkedList.reversePrint();
+
+doublyLinkedList.insertAtEnd(3);
+doublyLinkedList.print();
+doublyLinkedList.reversePrint();
+
+doublyLinkedList.insertAtEnd(7);
+doublyLinkedList.print();
+doublyLinkedList.reversePrint();
+
+System.out.println("============================");
+
+doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.insertAtPosition(2,1);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(3,2);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(7,1);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(8,2);
+doublyLinkedList.print();
+
+System.out.println("============================");
+
+doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.insertAtPosition(2,1);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(3,2);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(7,1);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(8,2);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(111,2);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(75,2);
+doublyLinkedList.print();
+
+doublyLinkedList.insertAtPosition(21,2);
+doublyLinkedList.print();
+
+System.out.println("Delete part: ");
+doublyLinkedList.deleteNthNode(1);//correct
+doublyLinkedList.print();
+doublyLinkedList.deleteNthNode(3);
+doublyLinkedList.print();
+doublyLinkedList.deleteNthNode(5);
+doublyLinkedList.print();
 }
 }
